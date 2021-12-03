570d286c400bb93757d875646c027d817f8bcc8c
Lucas Mofardini <lucasmofardini@gmail.com>
2021-12-03 04:13:23 -0300

    Starting calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import { Button, Typography, Box, Container } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container fixed>
+        <Typography className="titulo-main" variant="h1">Calculator made in ReactJS</Typography>
+        <Box className="container-calc">
+          <Button variant="contained" color="primary">
+            1
+          </Button>
+          <Button variant="contained" color="primary">
+            2
+          </Button>
+          <Button variant="contained" color="primary">
+            3
+          </Button>
+          <Button variant="contained" color="primary">
+            4
+          </Button>
+        </Box>
+
+      </Container>
+    </>
   );
 }
 
